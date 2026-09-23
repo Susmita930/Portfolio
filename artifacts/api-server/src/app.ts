c83c2950f8323d435010b166fd06b01104f33d07
susmita930 <susmita120506@gmail.com>
2026-09-23 06:54:24 +0530

    Update portfolio design and projects

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Express } from "express";
+import { IncomingMessage, ServerResponse } from "node:http";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,14 +11,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: IncomingMessage & { id?: string | number | object; method?: string; url?: string }) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: ServerResponse & { statusCode?: number }) {
         return {
           statusCode: res.statusCode,
         };
